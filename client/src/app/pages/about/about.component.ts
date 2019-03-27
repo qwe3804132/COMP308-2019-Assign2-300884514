eb76891b771d13bbffea9d515714a399e1c92aa6
qwe3804132 <liupeiran9324@outlook.com>
2019-03-26 22:08:27 -0400

    add compnent and routers

--- client/src/app/pages/about/about.component.ts
+++ client/src/app/pages/about/about.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { BasePageComponent } from 'src/app/partials/base-page/base-page.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent extends BasePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(route:ActivatedRoute) { 
+    super(route)
+  }
 
   ngOnInit() {
   }
--- client/src/app/pages/products/products.component.ts
+++ client/src/app/pages/products/products.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { BasePageComponent } from 'src/app/partials/base-page/base-page.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent extends BasePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(route:ActivatedRoute ){ 
+    super(route);
+  }
 
   ngOnInit() {
   }
