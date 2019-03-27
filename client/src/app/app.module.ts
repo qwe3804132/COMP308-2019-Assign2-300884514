fa17b0ed4304b29fcbe83b8b41dc543889d355f3
qwe3804132 <liupeiran9324@outlook.com>
2019-03-26 21:50:44 -0400

    create bunch of pages using angular

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './partials/header/header.component';
 import { FooterComponent } from './partials/footer/footer.component';
+import { BasePageComponent } from './partials/base-page/base-page.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    BasePageComponent,
+    HomeComponent,
+    AboutComponent,
+    ProductsComponent,
+    ServicesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
--- client/src/app/partials/footer/footer.component.ts
+++ client/src/app/partials/footer/footer.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  year:number;
 
   constructor() { }
 
   ngOnInit() {
+    const date=new Date();
+    this.year=date.getFullYear();
   }
 
 }
