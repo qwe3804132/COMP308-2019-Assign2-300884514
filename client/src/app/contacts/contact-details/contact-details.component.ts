d154aa11a8c4f556dcd4dca003ada363fa950bcc
qwe3804132 <liupeiran9324@outlook.com>
2019-03-30 10:34:22 -0400

    add added function

--- client/src/app/contacts/contact-details/contact-details.component.ts
+++ client/src/app/contacts/contact-details/contact-details.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactListService } from 'src/app/services/contact-list.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router, ActivatedRoute } from '@angular/router';
 import { Contact } from 'src/app/models/contact';
+
 
 @Component({
   selector: 'app-contact-details',
@@ -10,14 +14,37 @@ export class ContactDetailsComponent implements OnInit {
 title:string;
 contact:Contact
 
-  constructor() { }
+  constructor(
+    private activatedRoute:ActivatedRoute,
+    private flashMessage: FlashMessagesService,
+    private contactListService:ContactListService,
+    private router: Router
+    ) { }
 
   ngOnInit() {
+    this.title=this.activatedRoute.snapshot.data.title;
     this.contact=new Contact();
   }
   
   private onDeatilsPageSubmit():void{
+switch(this.title){
+  case 'Add Contact':
+  this.contactListService.addContact(this.contact).subscribe(data=>{
+    if(data.success){
+      this.flashMessage.show(data.msg,{cssClass:'alert-success',timeOut:3000});
+      this.router.navigate(['/contact/contact-list']);
 
+    }else{
+      this.flashMessage.show('Add Contact Failed',{cssClass:'alert-danger',timeOut:3000});
+      this.router.navigate(['/contact/contact-list']);
+
+    }
+  });
+  break;
+  
+  case 'Edit Contact':
+  break;
+}
   }
 
 }
