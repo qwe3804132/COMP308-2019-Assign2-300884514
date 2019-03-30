331d55f0bd82f80e5e6e963b4f8637ba541a38a9
qwe3804132 <liupeiran9324@outlook.com>
2019-03-30 18:33:37 -0400

    ready the personal pages

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,6 @@
 module.exports = {
-   "URI" : "mongodb://localhost/demo",
+   //"URI" : "mongodb://localhost/demo",
+   "URI":"mongodb://liupeiran9324:z1324356@ds058739.mlab.com:58739/comp308-w2019-lesson4a",
+
    "secret":"SomeSecret"
 }
