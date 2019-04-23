fe9984ffe6e3990befa592358b5f865ba395cd0d
qwe3804132 <36111045+qwe3804132@users.noreply.github.com>
2019-04-23 19:08:46 -0400

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-   "URI":"   mongodb://qwe3804132:z1324356@ds058739.mlab.com:58739/comp308-w2019-lesson4a"
+   "URI":"mongodb://qwe3804132:z1324356@ds058739.mlab.com:58739/comp308-w2019-lesson4a"
 
    "secret":"SomeSecret"
 }
